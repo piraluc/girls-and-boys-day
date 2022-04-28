db5ee26ffc5cc767edceb3cd3656c1be6bc37159
Luca Piras <luca.piras@outlook.com>
2022-04-28 11:51:58 +0200

    Convert chars to int the right way :)

--- args.c
+++ args.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]) {
-	// Convert the argument from characters to numbers
-	int x = charToInt(argv[1]);
-	int y = charToInt(argv[2]);
+	// Convert the two arguments from characters to numbers
+	int x = (long)argv[1];
+	int y = (long)argv[2];
 
 	// Add the two numbers
 	int z = x + y;
